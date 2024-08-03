7341ac0f2c4260895ea4ef378435d0e3e1895940
Codewithkrishna98 <143336373+Codewithkrishna98@users.noreply.github.com>
2024-08-03 18:44:10 +0530

    env

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async ()=>{
   try {
-     const connected = await mongoose.connect("mongodb+srv://monu:mg6677@cluster3.5eudgqq.mongodb.net/")
+     const connected = await mongoose.connect(process.env.DB_URL)
      console.log(`database is connected ${connected.connection.host}`)
   } catch (error) {
     console.log("database connection failed ", error);
